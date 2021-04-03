1b985ebe8a778e4f95b2f8c7a5b115d4c379b651
Isaac Serrano <serranoie99@gmail.com>
2021-04-02 21:05:59 -0700

    Diseno de reporte hecho

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
 import * as Location from 'expo-location';
@@ -78,6 +78,66 @@ export default function Add({ navigation }) {
 	}
 	return (
 		<View style={{ flex: 1 }}>
+			<Camera ref={(ref) => setCamera(ref)} style={styles.fixedRatio} type={type} />
+
+			<View
+				style={{
+					backgroundColor: '#000',
+					flex: 0.4,
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+			>
+				<TouchableOpacity
+					style={{ alignSelf: 'flex-end', flex: 0.1, position: 'absolute' }}
+					onPress={() => {
+						setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back);
+					}}
+				>
+					<Ionicons
+						style={{ marginRight: 40, paddingBottom: 10, color: 'white' }}
+						name="ios-reverse-camera"
+						size={38}
+						color="white"
+					/>
+				</TouchableOpacity>
+
+				<TouchableOpacity
+					style={{
+						flex: 0.1,
+						alignSelf: 'flex-start',
+						alignItems: 'center',
+						position: 'absolute',
+					}}
+					onPress={() => {
+						setFlash(
+							flash === Camera.Constants.FlashMode.torch
+								? Camera.Constants.FlashMode.torch
+								: Camera.Constants.FlashMode.off
+						);
+					}}
+				>
+					<Ionicons
+						style={{ marginLeft: 40, paddingBottom: 10, color: 'white' }}
+						name={flash ? 'ios-flash' : 'ios-flash-off'}
+						size={34}
+						color="white"
+					/>
+				</TouchableOpacity>
+
+				<TouchableOpacity stlye={styles.grabar} onPress={() => takePicture()}>
+					<Ionicons style={{ marginBottom: 10, color: '#ef3340' }} name="ios-radio-button-on" size={100} />
+				</TouchableOpacity>
+
+				<Text style={{ color: 'white' }}>
+					<Ionicons style={{ color: 'white' }} name="ios-navigate" size={15} color="white" />
+					{' ' + text}
+				</Text>
+
+				<Text style={{ marginTop: 10, fontSize: 14, color: 'white' }}>Explicanos en 30 segundos que sucede...</Text>
+			</View>
+
+			{/*
 			<View style={styles.cameraContainer}>
 				<Camera ref={(ref) => setCamera(ref)} style={styles.fixedRatio} type={type} ratio={'1:1'} />
 			</View>
@@ -91,7 +151,9 @@ export default function Add({ navigation }) {
 			<Button title="Take Picture" onPress={() => takePicture()} />
 			<Button title="Pick Image From Gallery" onPress={() => pickImage()} />
 			<Button title="Save" onPress={() => navigation.navigate('Save', { image })} />
+
 			{image && <Image source={{ uri: image }} style={{ flex: 1 }} />}
+            */}
 		</View>
 	);
 }
