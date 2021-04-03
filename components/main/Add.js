c152c6f9981bc4a1920b142f69b4981c5d662969
Isaac Serrano <serranoie99@gmail.com>
2021-04-02 22:07:44 -0700

    Al tomar foto ahora abre modal

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,9 +1,20 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, Button, Image, TouchableOpacity, Modal } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	Image,
+	TouchableOpacity,
+	Modal,
+	TextInput,
+	TouchableHighlight,
+} from 'react-native';
 import { Camera } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
 import * as Location from 'expo-location';
 import { Ionicons, FontAwesome5 } from '@expo/vector-icons';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function Add({ navigation }) {
 	const camRef = useRef(null);
@@ -17,6 +28,7 @@ export default function Add({ navigation }) {
 	const [errorMsg, setErrorMsg] = useState(null);
 	const [recording, setRecording] = useState(false);
 	const [videoCapturado, setVideoCapturado] = useState(null);
+	const [abrirModal, setAbrirModal] = useState(false);
 
 	// Get location
 	useEffect(() => {
@@ -54,6 +66,7 @@ export default function Add({ navigation }) {
 		if (camera) {
 			const data = await camera.takePictureAsync(null);
 			setImage(data.uri);
+			setAbrirModal(true);
 		}
 	};
 
@@ -154,6 +167,99 @@ export default function Add({ navigation }) {
 
 				<Text style={{ marginTop: 10, fontSize: 14, color: 'white' }}>Explicanos en 30 segundos que sucede...</Text>
 			</View>
+			{image && (
+				<Modal animationType="slide" transparent={true} visible={abrirModal}>
+					<View style={{ flex: 1 }}>
+						<View style={styles.centeredView}>
+							<View style={styles.modalView}>
+								<Text style={styles.modalText}>Datos necesarios del reporte</Text>
+
+								<DropDownPicker
+									items={[
+										{ label: 'angellist', value: 'angellist' },
+										{ label: 'codepen', value: 'codepen' },
+										{ label: 'envelope', value: 'envelope' },
+										{ label: 'etsy', value: 'etsy' },
+										{ label: 'facebook', value: 'faceboo' },
+										{ label: 'foursquare', value: 'foursquare' },
+										{ label: 'github-alt', value: 'github-alt' },
+										{ label: 'github', value: 'github' },
+										{ label: 'gitlab', value: 'gitlab' },
+										{ label: 'instagram', value: 'instagrama' },
+										{
+											label: 'Selecciona una empresa',
+											value: 'placeholder',
+											selected: true,
+											icon: () => <FontAwesome5 name="search" size={18} color="#000" />,
+											hidden: true,
+										},
+									]}
+									containerStyle={{ height: 40, alignSelf: 'stretch', marginBottom: 10 }}
+									style={{ backgroundColor: '#fafafa', alignSelf: 'center' }}
+									itemStyle={{
+										justifyContent: 'flex-start',
+									}}
+									labelStyle={{
+										textAlign: 'center',
+									}}
+									selectedLabelStyle={{ color: '#000000' }}
+									dropDownStyle={{ backgroundColor: '#fafafa' }}
+									dropDownMaxHeight={400}
+									onChangeItem={(item) => setValue(item.value)}
+									searchable={true}
+									searchablePlaceholder="Busca una ciudad"
+									searchablePlaceholderTextColor="gray"
+									searchableError={() => <Text>No existe una empresa con ese nombre</Text>}
+								/>
+
+								{/* TextInputs to specify which title and categories has */}
+								<View styles={styles.reportForms}>
+									<TextInput
+										style={styles.inputReport}
+										placeholder="Titulo del reporte"
+										underlineColorAndroid={'transparent'}
+									/>
+
+									<TextInput
+										style={styles.inputMultipleReport}
+										placeholder="Descripción del reporte"
+										multiline
+										numberOfLines={2}
+										underlineColorAndroid={'transparent'}
+									/>
+								</View>
+
+								{/* View to organize the buttons */}
+								<View style={styles.modalBotones}>
+									{/* Cancel button */}
+									<View style={styles.accionesBotones}>
+										<TouchableHighlight
+											style={{ ...styles.aceptarVideoBtn }}
+											onPress={() => {
+												setAbrirModal(false);
+
+												// Enviar datos al JSON correspondiente
+												sendInfoToJSON(videoCapturado);
+											}}
+										>
+											<Text style={{ color: 'white', fontWeight: 'bold', fontSize: 16 }}>Aceptar</Text>
+										</TouchableHighlight>
+
+										<TouchableHighlight
+											style={{ ...styles.cancelarVideoBtn }}
+											onPress={() => {
+												setAbrirModal(false);
+											}}
+										>
+											<Text style={{ color: '#c42525', fontWeight: 'bold', fontSize: 16 }}>Cancelar</Text>
+										</TouchableHighlight>
+									</View>
+								</View>
+							</View>
+						</View>
+					</View>
+				</Modal>
+			)}
 
 			{/*
 			<View style={styles.cameraContainer}>
@@ -184,5 +290,94 @@ const styles = StyleSheet.create({
 	fixedRatio: {
 		flex: 1,
 		aspectRatio: 1,
+	},
+
+	container: {
+		flex: 1,
+		backgroundColor: '#000',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	boton: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#121212',
+		margin: 20,
+		borderRadius: 10,
+		height: 50,
+	},
+	grabar: {
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	fondoVisualizacon: {
+		flex: 1,
+	},
+	accionesBotones: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		left: 0,
+		bottom: 0,
+	},
+	centeredView: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginTop: 22,
+	},
+	modalView: {
+		margin: 20,
+		backgroundColor: 'white',
+		borderRadius: 5,
+		padding: 35,
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 3.84,
+		elevation: 5,
+	},
+	modalText: {
+		marginBottom: 20,
+		fontWeight: 'bold',
+		fontSize: 22,
+		textAlign: 'center',
+	},
+	modalBotones: {
+		alignContent: 'center',
+	},
+	aceptarVideoBtn: {
+		backgroundColor: '#006600',
+		fontWeight: 'bold',
+		borderRadius: 3,
+		padding: 10,
+		paddingHorizontal: 40,
+	},
+	cancelarVideoBtn: {
+		backgroundColor: 'white',
+		padding: 10,
+	},
+	reportForms: {
+		alignSelf: 'stretch',
+	},
+	inputReport: {
+		alignSelf: 'stretch',
+		height: 40,
+		marginBottom: 12,
+		color: '#000',
+		borderColor: '#e0e0e0',
+		borderRadius: 5,
+		borderWidth: 1,
+	},
+	inputMultipleReport: {
+		alignSelf: 'stretch',
+		height: 65,
+		marginBottom: 12,
+		color: '#000',
+		borderColor: '#e0e0e0',
+		borderRadius: 5,
+		borderWidth: 1,
 	},
 });
