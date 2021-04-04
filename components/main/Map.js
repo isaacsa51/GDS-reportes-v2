10149e030575e922dd5d86f2fb22524d30909f1f
Isaac Serrano <serranoie99@gmail.com>
2021-04-04 13:38:03 -0600

    Markers tienen tooltips con una breve descripción

--- components/main/Map.js
+++ components/main/Map.js
@@ -1,109 +1,119 @@
 import React, { useState, useEffect, useRef } from 'react';
 import {
-	Alert,
-	StyleSheet,
-	Text,
-	View,
-	SafeAreaView,
-	TouchableOpacity,
-	Dimensions,
-	ActivityIndicator,
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Dimensions,
+  ActivityIndicator,
 } from 'react-native';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import firebase from 'firebase';
 require('firebase/firestore');
 import { connect } from 'react-redux';
 
 let initialState = {
-	longitude: null,
-	latitude: null,
-	latitudeDelta: 0.035,
-	longitudeDelta: 0.035,
+  longitude: null,
+  latitude: null,
+  latitudeDelta: 0.025,
+  longitudeDelta: 0.025,
 };
 
 const Map = (props) => {
-	const [currentPosition, setCurrentPosition] = useState(initialState);
-	const [location, setLocation] = useState(null);
-	const [errorMsg, setErrorMsg] = useState(null);
-	const [reports, setReports] = useState([]);
+  const [currentPosition, setCurrentPosition] = useState(initialState);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [reports, setReports] = useState([]);
 
-	useEffect(() => {
-		if (props.usersFollowingLoaded == props.following.length && props.following.length !== 0) {
-			props.feed.sort(function (x, y) {
-				return x.creation - y.creation;
-			});
-			setReports(props.feed);
-		}
-	}, [props.usersFollowingLoaded, props.feed]);
+  useEffect(() => {
+    if (props.usersFollowingLoaded == props.following.length && props.following.length !== 0) {
+      props.feed.sort(function (x, y) {
+        return x.creation - y.creation;
+      });
+      setReports(props.feed);
+    }
+  }, [props.usersFollowingLoaded, props.feed]);
 
-	// Get location
-	useEffect(() => {
-		navigator.geolocation.getCurrentPosition(
-			(position) => {
-				const { longitude, latitude } = position.coords;
+  // Get location
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { longitude, latitude } = position.coords;
 
-				setCurrentPosition({
-					...currentPosition,
-					latitude,
-					longitude,
-				});
-			},
-			(error) => alert(error.message),
-			{ timeout: 20000, maximumAge: 1000 }
-		);
-	}, []);
+        setCurrentPosition({
+          ...currentPosition,
+          latitude,
+          longitude,
+        });
+      },
+      (error) => alert(error.message),
+      { timeout: 20000, maximumAge: 1000 }
+    );
+  }, []);
 
-	const map = useRef(null);
+  const map = useRef(null);
 
-	return currentPosition.latitude ? (
-		<SafeAreaView style={styles.container}>
-			<MapView
-				map={map}
-				provider={MapView.PROVIDER_GOOGLE}
-				style={styles.map}
-				loadingEnabled={true}
-				showsUserLocation={true}
-				region={currentPosition}
-			>
-				{reports.map((marker, index) => (
-					<Marker
-						key={index}
-						coordinate={{ latitude: marker.location.coords.latitude, longitude: marker.location.coords.longitude }}
-						title={marker.title}
-						description={marker.caption}
-					/>
-				))}
-			</MapView>
-		</SafeAreaView>
-	) : (
-		<ActivityIndicator
-			size="large"
-			style={{
-				justifyContent: 'center',
-				alignSelf: 'center',
-				alignContent: 'center',
-			}}
-		/>
-	);
+  return currentPosition.latitude ? (
+    <SafeAreaView style={styles.container}>
+      <MapView
+        map={map}
+        provider={MapView.PROVIDER_GOOGLE}
+        style={styles.map}
+        loadingEnabled={true}
+        showsUserLocation={true}
+        region={currentPosition}
+      >
+        {reports.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={{ latitude: marker.location.coords.latitude, longitude: marker.location.coords.longitude }}
+            title={marker.title}
+            description={marker.caption}
+          >
+            <Callout tooltip={false}>
+              <View style={{ height: '60%' }}>
+                <Text style={{ fontWeight: 'bold', fontSize: 20 }}>
+                  Autor: {marker.user.name} {marker.user.lastName}
+                </Text>
+                <Text style={{ fontSize: 16 }}>Titulo: {marker.caption}</Text>
+                <Text style={{ fontSize: 16, opacity: 0.5, fontStyle: 'italic' }}>Status: {marker.status}</Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
+    </SafeAreaView>
+  ) : (
+    <ActivityIndicator
+      size="large"
+      style={{
+        justifyContent: 'center',
+        alignSelf: 'center',
+        alignContent: 'center',
+      }}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-	},
-	map: {
-		width: Dimensions.get('window').width,
-		height: Dimensions.get('window').height,
-	},
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
 });
 
 const mapStateToProps = (store) => ({
-	currentUser: store.userState.currentUser,
-	following: store.userState.following,
-	feed: store.usersState.feed,
-	usersFollowingLoaded: store.usersState.usersFollowingLoaded,
+  currentUser: store.userState.currentUser,
+  following: store.userState.following,
+  feed: store.usersState.feed,
+  usersFollowingLoaded: store.usersState.usersFollowingLoaded,
 });
 
 export default connect(mapStateToProps, null)(Map);
