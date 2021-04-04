c786a9537383a332496053f27242e22a75124b52
Isaac Serrano <serranoie99@gmail.com>
2021-04-04 11:56:51 -0600

    Pantalla de mapa ahora renderiza puntos de reportes

--- components/main/Map.js
+++ components/main/Map.js
@@ -1,80 +1,109 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from 'react';
 import {
-  Alert,
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  TouchableOpacity,
-  Dimensions,
-  ActivityIndicator,
-} from "react-native";
-import * as Location from "expo-location";
-import * as Permissions from "expo-permissions";
-import MapView from "react-native-maps";
+	Alert,
+	StyleSheet,
+	Text,
+	View,
+	SafeAreaView,
+	TouchableOpacity,
+	Dimensions,
+	ActivityIndicator,
+} from 'react-native';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import MapView, { Marker } from 'react-native-maps';
+import firebase from 'firebase';
+require('firebase/firestore');
+import { connect } from 'react-redux';
 
 let initialState = {
-  longitude: null,
-  latitude: null,
-  latitudeDelta: 0.035,
-  longitudeDelta: 0.035,
+	longitude: null,
+	latitude: null,
+	latitudeDelta: 0.035,
+	longitudeDelta: 0.035,
 };
 
-const Map = () => {
-  const [currentPosition, setCurrentPosition] = useState(initialState);
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+const Map = (props) => {
+	const [currentPosition, setCurrentPosition] = useState(initialState);
+	const [location, setLocation] = useState(null);
+	const [errorMsg, setErrorMsg] = useState(null);
+	const [reports, setReports] = useState([]);
 
-  // Get location
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { longitude, latitude } = position.coords;
+	useEffect(() => {
+		if (props.usersFollowingLoaded == props.following.length && props.following.length !== 0) {
+			props.feed.sort(function (x, y) {
+				return x.creation - y.creation;
+			});
+			setReports(props.feed);
+		}
+	}, [props.usersFollowingLoaded, props.feed]);
 
-        setCurrentPosition({
-          ...currentPosition,
-          latitude,
-          longitude,
-        });
-      },
-      (error) => alert(error.message),
-      { timeout: 20000, maximumAge: 1000 }
-    );
-  }, []);
+	// Get location
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				const { longitude, latitude } = position.coords;
 
-  const map = useRef(null);
+				setCurrentPosition({
+					...currentPosition,
+					latitude,
+					longitude,
+				});
+			},
+			(error) => alert(error.message),
+			{ timeout: 20000, maximumAge: 1000 }
+		);
+	}, []);
 
-  return currentPosition.latitude ? (
-    <SafeAreaView style={styles.container}>
-      <MapView
-        map={map}
-        provider={MapView.PROVIDER_GOOGLE}
-        style={styles.map}
-        loadingEnabled={true}
-        showsUserLocation={true}
-        region={currentPosition}
-      />
-    </SafeAreaView>
-  ) : (
-    <ActivityIndicator
-      size="large"
-      style={{
-        justifyContent: "center",
-        alignSelf: "center",
-        alignContent: "center",
-      }}
-    />
-  );
+	const map = useRef(null);
+
+	return currentPosition.latitude ? (
+		<SafeAreaView style={styles.container}>
+			<MapView
+				map={map}
+				provider={MapView.PROVIDER_GOOGLE}
+				style={styles.map}
+				loadingEnabled={true}
+				showsUserLocation={true}
+				region={currentPosition}
+				markers={reports.map((marker) => {
+					return (
+						<Marker
+							key={marker.titulo}
+							description={marker.caption}
+							coordinate={{ latitude: marker.location.coords.latitude, longitude: marker.location.coords.longitude }}
+						/>
+					);
+				})}
+			/>
+		</SafeAreaView>
+	) : (
+		<ActivityIndicator
+			size="large"
+			style={{
+				justifyContent: 'center',
+				alignSelf: 'center',
+				alignContent: 'center',
+			}}
+		/>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
-  },
+	container: {
+		flex: 1,
+	},
+	map: {
+		width: Dimensions.get('window').width,
+		height: Dimensions.get('window').height,
+	},
 });
 
-export default Map;
+const mapStateToProps = (store) => ({
+	currentUser: store.userState.currentUser,
+	following: store.userState.following,
+	feed: store.usersState.feed,
+	usersFollowingLoaded: store.usersState.usersFollowingLoaded,
+});
+
+export default connect(mapStateToProps, null)(Map);
